4684c3dce9b28ad92588f7d2d3bcf1abbe0504d5
Yuki Nishida <nishida@rsworks.jp>
2022-11-09 14:09:45 +0900

    init

---
+++ plugins/plugin_icon.ml
@@ -0,0 +1,45 @@
+open Why3
+
+let find_target (ds : Decl.decl list) : Ident.ident option =
+  let is_tuple ty =
+    match ty.Ty.ty_node with
+    | Tyapp (s, _) ->
+        Re.(execp (compile (seq [ bos; alt [ str "tuple"; str "or" ] ])))
+          s.ts_name.id_string
+    | _ -> false
+  in
+  List.find_map
+    (fun d ->
+      match d.Decl.d_node with
+      | Dparam p ->
+          Option.bind p.ls_value (fun ty ->
+              if is_tuple ty then Some p.ls_name else None)
+      | _ -> None)
+    ds
+
+let destruct_adt env =
+  let rec aux t =
+    let x = find_target @@ Task.task_decls t in
+    match x with
+    | None -> [ t ]
+    | Some x ->
+        let naming_table = Args_wrapper.build_naming_tables t in
+        let x = Ident.id_unique naming_table.printer x in
+        Trans.apply_transform_args "destruct_term_subst" env [ x ] naming_table
+          Lexer.whyml_format t
+        |> List.map aux |> List.flatten
+  in
+  Trans.store aux
+
+let simplify env : Task.task Trans.tlist =
+  let split_vc = Trans.lookup_transform_l "split_vc" env in
+  let simplify_computations =
+    Trans.singleton @@ Trans.lookup_transform "simplify_computations" env
+  in
+  let inline_all = Trans.singleton Inlining.all in
+  let subst_all = Trans.singleton Subst.subst_all in
+  Trans.seq_l
+    [ inline_all; split_vc; subst_all; destruct_adt env; simplify_computations ]
+
+let () =
+  Trans.register_env_transform_l ~desc:"icon michelson" "icon_simplify" simplify
